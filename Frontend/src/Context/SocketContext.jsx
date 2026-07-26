cabb1f98be9489730be4889937964769111fff5c
Snigdha Saxena <snigdhasaxena2@gmail.com>
2026-07-26 14:43:18 +0530

    Updated backend URLs for deployment

--- Frontend/src/Context/SocketContext.jsx
+++ Frontend/src/Context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io('http://localhost:5000',{
+            const socket = io('https://chat-app-p7xy.onrender.com',{
                 query: {
                     userId: authUser._id
                 }
